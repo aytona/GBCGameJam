d6903a7e4a36095aab3a9357a9740ce5ee503a15
Christopher Aytona <christopher.aytona@gmail.com>
2016-06-25 12:17:39 -0400

    MenuUI button functionalities

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour {
+
+	public GameObject[] screenObjs;
+
+	public void Play()
+	{
+		Application.LoadLevel("Game");
+	}
+
+	public void ScreenTransition()
+	{
+		string name = EventSystem.current.currentSelectedGameObject.tag;
+		ScreenChange(name);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+	private void ScreenChange(string name)
+	{
+		foreach(GameObject i in screenObjs)
+		{
+			if (i.name != name)
+			{
+				i.SetActive(false);
+			}
+			else
+			{
+				i.SetActive(true);
+			}
+		}
+	}
+}
