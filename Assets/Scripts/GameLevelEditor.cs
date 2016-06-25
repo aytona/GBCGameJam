d4b6f6a5cbc278e3a62caf1141d80a94c5373f38
Christopher Aytona <christopher.aytona@gmail.com>
2016-06-25 18:07:20 -0400

    Getting the size of the models/mesh

--- Assets/Scripts/GameLevelEditor.cs
+++ Assets/Scripts/GameLevelEditor.cs
@@ -5,8 +5,8 @@ using System.Collections;
 public class GameLevelEditor : EditorWindow {
 
 	private int numberOfModels = 1;
-	private int minModels;
-	private int maxModels;
+	private Vector2 Xminmax;
+	private Vector2 Yminmax;
 	private GameObject[] modelsArray = new GameObject[1];
 
 	[MenuItem("LevelEditor/CityGenerator")]
@@ -17,11 +17,8 @@ public class GameLevelEditor : EditorWindow {
 
 	void OnGUI()
 	{
-		GUILayout.Label("Min Models", EditorStyles.boldLabel);
-		minModels = EditorGUILayout.IntField(minModels, GUILayout.Width(30));
-
-		GUILayout.Label("MaxModels", EditorStyles.boldLabel);
-		maxModels = EditorGUILayout.IntField(maxModels, GUILayout.Width(30));
+		Xminmax = EditorGUILayout.Vector2Field("X min and max", Xminmax);
+		Yminmax = EditorGUILayout.Vector2Field("Y min and max", Yminmax);
 
 		GUILayout.Label("Number of Models", EditorStyles.boldLabel);
 		numberOfModels = (int)GUILayout.HorizontalScrollbar(numberOfModels, 1.0f, 1.0f, 5.0f);
@@ -33,18 +30,18 @@ public class GameLevelEditor : EditorWindow {
 		}
 		for(int i = 0; i < modelsArray.Length; i++)
 		{
-			modelsArray[i] = EditorGUILayout.ObjectField(modelsArray[i], typeof(GameObject), true) as GameObject;
+			modelsArray[i] = EditorGUILayout.ObjectField(modelsArray[i], typeof(GameObject), false) as GameObject;
 		}
 		if (GUILayout.Button("Generate"))
 		{
-			if (minModels == 0 || maxModels == 0)
+			if (Xminmax.y == 0 || Yminmax.y == 0)
 			{
-				ShowNotification(new GUIContent("Number must be nonzero"));
+				ShowNotification(new GUIContent("Max numbers must be nonzero"));
 				return;
 			}
-			if (minModels > maxModels)
+			if (Xminmax.x > Xminmax.y || Yminmax.x > Yminmax.y)
 			{
-				ShowNotification(new GUIContent("Min Models must be smaller than Max Models"));
+				ShowNotification(new GUIContent("Min numbers must be smaller than Max numbers"));
 				return;
 			}
 			foreach(GameObject i in modelsArray)
@@ -54,13 +51,22 @@ public class GameLevelEditor : EditorWindow {
 					ShowNotification (new GUIContent("Each array must be filled"));
 					return;
 				}
-				GenerateLevel(minModels, maxModels, modelsArray);
 			}
+
+			GenerateLevel(Xminmax.x, Xminmax.y, Yminmax.x, Yminmax.y, modelsArray);
 		}
 	}
 
-	private static void GenerateLevel(int min, int max, GameObject[] models)
+	private static void GenerateLevel(float xMin, float xMax, float yMin, float yMax, GameObject[] models)
 	{
-
+		GameObject rootObject = new GameObject();
+		rootObject.name = "Level";
+		//Vector3 currentLocation = new Vector3(xMin, 0, yMin);
+		float[] modelWidths = new float[models.Length];
+		for (int i = 0; i < models.Length; i++)
+		{
+			modelWidths[i] = 
+				(models[i].GetComponent<MeshFilter>().mesh.bounds.size.x * 2.0f); // Multiply by 2 because its the X-length from the center
+		}
 	}
 }
