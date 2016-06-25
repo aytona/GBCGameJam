89b26f225e3c32fb1368ef7a636d5def4c2278ff
Christopher Aytona <christopher.aytona@gmail.com>
2016-06-25 14:49:56 -0400

    LevelEditor start

---
+++ Assets/Scripts/GameLevelEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GameLevelEditor : EditorWindow {
+
+	private int numberOfModels;
+
+	[MenuItem("LevelEditor/CityGenerator")]
+	public static void OpenLevelEditorWindow()
+	{
+		EditorWindow.GetWindow<GameLevelEditor>(true, "City Generator");
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label("Number of Models", EditorStyles.boldLabel);
+		numberOfModels = (int)GUILayout.HorizontalScrollbar(numberOfModels, 1.0f, 0.0f, 20.0f);
+
+		GUILayout.Label("Model Prefabs", EditorStyles.boldLabel);
+		GameObject[] modelsArray = new GameObject[numberOfModels];
+		foreach(GameObject i in modelsArray)
+		{
+			EditorGUILayout.ObjectField(i, typeof(GameObject), true);
+		}
+
+		if (GUILayout.Button("Generate"))
+		{
+
+		}
+	}
+}
