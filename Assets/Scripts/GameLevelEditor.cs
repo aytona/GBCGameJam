1f3f580ac93d8dccad9d26a6398a948eb444d06d
Christopher Aytona <christopher.aytona@gmail.com>
2016-06-25 16:16:07 -0400

    min and max of models, and other small changes

--- Assets/Scripts/GameLevelEditor.cs
+++ Assets/Scripts/GameLevelEditor.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class GameLevelEditor : EditorWindow {
 
 	private int numberOfModels = 1;
+	private int minModels;
+	private int maxModels;
 	private GameObject[] modelsArray = new GameObject[1];
 
 	[MenuItem("LevelEditor/CityGenerator")]
@@ -15,11 +17,16 @@ public class GameLevelEditor : EditorWindow {
 
 	void OnGUI()
 	{
+		GUILayout.Label("Min Models", EditorStyles.boldLabel);
+		minModels = EditorGUILayout.IntField(minModels, GUILayout.Width(30));
+
+		GUILayout.Label("MaxModels", EditorStyles.boldLabel);
+		maxModels = EditorGUILayout.IntField(maxModels, GUILayout.Width(30));
+
 		GUILayout.Label("Number of Models", EditorStyles.boldLabel);
-		numberOfModels = (int)GUILayout.HorizontalScrollbar(numberOfModels, 1.0f, 1.0f, 20.0f);
+		numberOfModels = (int)GUILayout.HorizontalScrollbar(numberOfModels, 1.0f, 1.0f, 5.0f);
 
 		GUILayout.Label("Model Prefabs", EditorStyles.boldLabel);
-
 		if (modelsArray.Length != numberOfModels)
 		{
 			modelsArray = new GameObject[numberOfModels];
@@ -30,9 +37,14 @@ public class GameLevelEditor : EditorWindow {
 		}
 		if (GUILayout.Button("Generate"))
 		{
-			if (numberOfModels == 0)
+			if (minModels == 0 || maxModels == 0)
 			{
-				ShowNotification (new GUIContent("Number of models must be nonzero"));
+				ShowNotification(new GUIContent("Number must be nonzero"));
+				return;
+			}
+			if (minModels > maxModels)
+			{
+				ShowNotification(new GUIContent("Min Models must be smaller than Max Models"));
 				return;
 			}
 			foreach(GameObject i in modelsArray)
@@ -42,7 +54,13 @@ public class GameLevelEditor : EditorWindow {
 					ShowNotification (new GUIContent("Each array must be filled"));
 					return;
 				}
+				GenerateLevel(minModels, maxModels, modelsArray);
 			}
 		}
+	}
+
+	private static void GenerateLevel(int min, int max, GameObject[] models)
+	{
+
 	}
 }
