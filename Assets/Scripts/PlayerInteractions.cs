c5e622900a70da613d256ac675a24829ac2e4d0a
Christopher Aytona <christopher.aytona@gmail.com>
2016-06-25 13:51:55 -0400

    Player interactions testing

---
+++ Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractions : MonoBehaviour {
+
+	public GameObject buttonPrompt;
+
+	private bool nearInteractable;
+
+	void Update()
+	{
+		if (nearInteractable)
+		{
+			buttonPrompt.SetActive(true);
+		}
+		else if (!nearInteractable)
+		{
+			buttonPrompt.SetActive(false);
+		}
+	}
+
+	#region Triggers
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Interactable")
+		{
+			nearInteractable = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Interactable")
+		{
+			nearInteractable = false;
+		}
+	}
+	#endregion
+}
--- Assets/UnityChan/Scripts/ThirdPersonCamera.cs
+++ Assets/UnityChan/Scripts/ThirdPersonCamera.cs
@@ -38,23 +38,23 @@ public class ThirdPersonCamera : MonoBehaviour
 	void FixedUpdate ()	// このカメラ切り替えはFixedUpdate()内でないと正常に動かない
 	{
 		
-		if(Input.GetButton("Fire1"))	// left Ctlr
-		{	
-			// Change Front Camera
-			setCameraPositionFrontView();
-		}
-		
-		else if(Input.GetButton("Fire2"))	//Alt
-		{	
-			// Change Jump Camera
-			setCameraPositionJumpView();
-		}
-		
-		else
-		{	
+//		if(Input.GetButton("Fire1"))	// left Ctlr
+//		{	
+//			// Change Front Camera
+//			setCameraPositionFrontView();
+//		}
+//		
+//		else if(Input.GetButton("Fire2"))	//Alt
+//		{	
+//			// Change Jump Camera
+//			setCameraPositionJumpView();
+//		}
+//		
+//		else
+//		{	
 			// return the camera to standard position and direction
 			setCameraPositionNormalView();
-		}
+//		}
 	}
 
 	void setCameraPositionNormalView()
